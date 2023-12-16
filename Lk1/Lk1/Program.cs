3e4827b3e0f4ad071027023e4ff9ad551754c26d
Stanislav Larionov <101111909+Lavrysha28@users.noreply.github.com>
2023-12-16 15:54:55 +0200

    Add files via upload

---
+++ Lk1/Lk1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // iнiцiалiзацiя генератора псевдовипадкових чисел
+        Random random = new Random();
+
+        // Генерацiя та виведення 10 псевдовипадкових чисел
+        Console.WriteLine("Послiдовнiсть псевдовипадкових чисел:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            int randomNumber = random.Next();
+            Console.WriteLine(randomNumber);
+        }
+    }
+}
