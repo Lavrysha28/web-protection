2af2a5a5ae1ca8018f50ab9b7c39d68cc608b15b
Stanislav Larionov <101111909+Lavrysha28@users.noreply.github.com>
2023-12-16 22:15:11 +0200

    Add files via upload

---
+++ Lk1.2/Lk1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        // iнiцiалiзацiя генератора криптографiчно стiйких випадкових чисел
+        using (RNGCryptoServiceProvider rngCryptoServiceProvider = new RNGCryptoServiceProvider())
+        {
+            // Генерацiя та виведення 10 криптографiчно стiйких випадкових чисел
+            Console.WriteLine("Криптографiчно стiйка послiдовнiсть випадкових чисел:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                byte[] randomNumber = new byte[4]; // Наприклад, 4 байти для отримання 32 бiт
+                rngCryptoServiceProvider.GetBytes(randomNumber);
+
+                int randomInt = BitConverter.ToInt32(randomNumber, 0);
+                Console.WriteLine(randomInt);
+            }
+        }
+    }
+}
